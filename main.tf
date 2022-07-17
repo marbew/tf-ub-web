292762f595e0174560ba335c36fc511a714e3a6e
Marbew <lixundot@gmail.com>
2022-07-17 02:03:25 +0100

    webserver security group

--- main.tf
+++ main.tf
@@ -59,3 +59,20 @@ resource "aws_security_group" "tf-ubuntu_sg_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "web-sg" {
+  name = "webserver"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
