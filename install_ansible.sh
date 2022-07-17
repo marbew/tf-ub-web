afb41e31217ac1d1e958f4f16f85b66703a75efe
MarBEW <lixundot@gmail.com>
2022-07-17 03:07:47 +0100

    Delete install_ansible.sh

--- install_ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt install ansible -y
-git clone https://github.com/marbew/tf-ub-web.git
-sudo sed -i '/^#inventory.*  /s/^#//' /etc/ansible/ansible.cfg
-sudo echo "localhost ansible_connection=local" >> /etc/ansbile/hosts
-ansible-playbook Setup_Apache_on_EC2_with_Terraform/apache.yml 
